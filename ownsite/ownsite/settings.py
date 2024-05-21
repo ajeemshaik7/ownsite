a27d1a88e63ad4ffb22871f7e25193a0695b9bd6
Ajeem shaik <132434412+ajeemshaik7@users.noreply.github.com>
2024-05-21 20:24:49 +0530

    Update settings.py

--- ownsite/ownsite/settings.py
+++ ownsite/ownsite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-wt#4@+(n!-^e*7!ggb+&^-sly+6n8cgfn191&(6l^a849*dmf5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
+ALLOWED_HOSTS = [ '.vercel.app']
 
 
 # Application definition
